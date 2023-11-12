a8311bdbd103c20f6f4d0e898f2a243a4f708637
congdilema <86637974+congdilema@users.noreply.github.com>
2023-11-12 22:42:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/roommate/BackKeyHandler.java
@@ -0,0 +1,35 @@
+package com.example.roommate;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+public class BackKeyHandler {
+
+    private long backKeyPressedTime = 0;
+    private Activity activity;
+    private Toast toast;
+
+    public BackKeyHandler(Activity activity) {
+        this.activity = activity;
+    }
+
+    private void showGuide(){
+        toast = Toast.makeText(activity,"한 번 더 누르면 종료됩니다.",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void onBackPressed(double time) {
+        if (System.currentTimeMillis() > backKeyPressedTime + (time * 1000)) {
+            backKeyPressedTime = System.currentTimeMillis();
+            showGuide();
+            return;
+        }
+
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            activity.moveTaskToBack(true);
+            activity.finishAndRemoveTask();
+            toast.cancel();
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/roommate/GroupCalendar/GroupCalendarList.java
@@ -0,0 +1,19 @@
+package com.example.roommate.GroupCalendar;
+
+public class GroupCalendarList {
+
+    String date;
+
+    GroupCalendarList(String date){
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+}
